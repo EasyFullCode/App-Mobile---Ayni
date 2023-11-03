a1e81dd11117f99dd679005d65258257bc4cfa02
Andre Pillihuaman <aphuachaca@gmail.com>
2023-11-02 19:54:03 -0500

    Home se cambio a ScrollLayout y se inició con Categorias

--- app/src/main/java/com/example/proyectoayni/MenuAyni.java
+++ app/src/main/java/com/example/proyectoayni/MenuAyni.java
@@ -46,7 +46,7 @@ public class MenuAyni extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_intercambios, R.id.nav_perfil, R.id.nav_publicar)
+                R.id.nav_home, R.id.nav_intercambios, R.id.nav_perfil, R.id.nav_publicar,R.id.nav_categorias)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu_ayni);
